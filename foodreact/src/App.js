68252b70115b6eb89ce45c2f6db8e666e23d2e36
Chintan Patel <patelchintan843@gmail.com>
2023-03-14 21:31:22 +0530

    initial commit

---
+++ foodreact/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Home from './screens/Home';
+
+function App() {
+  return (
+    <>
+      <div>hello</div>
+      <Navbar />
+      <Footer />
+      <Home />
+    </>
+  );
+}
+
+export default App;
